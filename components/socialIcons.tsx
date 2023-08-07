87ac20217c982f613fdc5c27de8c585cfdf13168
Kawan Mark Silva <128873271+KawanMark@users.noreply.github.com>
2023-08-07 00:59:01 -0300

    Update socialIcons.tsx

--- components/socialIcons.tsx
+++ components/socialIcons.tsx
@@ -1,31 +1,37 @@
-import { useRef } from "react"
-import { useState } from "react"
+import { useRef, useState } from "react";
 
-export default function IconButton({ children, text, color, link, ...props }) {
-  const [hovered, setHovered] = useState(false)
-  const ref = useRef(null)
+type IconButtonProps = {
+  children: React.ReactNode;
+  text: string;
+  link: string;
+  color?: string;
+};
+
+export default function IconButton({ children, text, color, link, ...props }: IconButtonProps) {
+  const [hovered, setHovered] = useState(false);
+  const ref = useRef(null);
 
   return (
     <a href={link} target="_blank" rel="noopener noreferrer">
-    <button
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-      className={`
-        flex p-3 gap-1 mx-3 items-center rounded-lg
-        text-white ${color || "bg-gray-600"}
-      `}
-      {...props}
-    >
-      {children}
-      <div
-        style={{ width: hovered ? ref.current?.offsetWidth || 0 : 0 }}
-        className="overflow-x-hidden transition-all duration-300 ease-out"
+      <button
+        onMouseEnter={() => setHovered(true)}
+        onMouseLeave={() => setHovered(false)}
+        className={`
+          flex p-3 gap-1 mx-3 items-center rounded-lg
+          text-white ${color || "bg-gray-600"}
+        `}
+        {...props}
       >
-        <span ref={ref} className="px-1.5">
-          {text}
-        </span>
-      </div>
-    </button>
+        {children}
+        <div
+          style={{ width: hovered ? ref.current?.offsetWidth || 0 : 0 }}
+          className="overflow-x-hidden transition-all duration-300 ease-out"
+        >
+          <span ref={ref} className="px-1.5">
+            {text}
+          </span>
+        </div>
+      </button>
     </a>
-  )
+  );
 }
